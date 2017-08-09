6965b64d98a91703c17aae77dd70682387ee8277
Igor Rodionov <goruha@gmail.com>
2017-08-10 01:20:33 +0600

    Implement EFS Module (#1)
    * Init
    
    * Use label module
    
    * Address PR comments
    
    * Cleanup module
    
    * Update main.tf
    
    * Address PR comments
    
    * Update README.md
    
    * Update LICENSE

---
+++ main.tf
@@ -0,0 +1,63 @@
+# Define composite variables for resources
+module "label" {
+  source    = "git::https://github.com/cloudposse/tf_label.git?ref=tags/0.1.0"
+  namespace = "${var.namespace}"
+  name      = "${var.name}"
+  stage     = "${var.stage}"
+}
+
+
+resource "aws_efs_file_system" "default" {
+  tags {
+    Name      = "${module.label.id}"
+    Namespace = "${var.namespace}"
+    Stage     = "${var.stage}"
+  }
+}
+
+resource "aws_efs_mount_target" "default" {
+  count           = "${length(var.availability_zones)}"
+  file_system_id  = "${aws_efs_file_system.default.id}"
+  subnet_id       = "${element(var.subnets, count.index)}"
+  security_groups = ["${aws_security_group.default.id}"]
+}
+
+resource "aws_security_group" "default" {
+  name        = "${module.label.id}"
+  description = "EFS"
+  vpc_id      = "${var.vpc_id}"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  ingress {
+    from_port       = "2049"                     # NFS
+    to_port         = "2049"
+    protocol        = "tcp"
+    security_groups = ["${var.security_groups}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name      = "${module.label.id}"
+    Namespace = "${var.namespace}"
+    Stage     = "${var.stage}"
+  }
+}
+
+module "dns" {
+  source    = "git::https://github.com/cloudposse/tf_hostname.git?ref=tags/0.1.0"
+  namespace = "${var.namespace}"
+  name      = "${var.name}"
+  stage     = "${var.stage}"
+  ttl       = 60
+  zone_id   = "${var.zone_id}"
+  records   = ["${aws_efs_file_system.default.id}.efs.${var.aws_region}.amazonaws.com"]
+}
---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "namespace" {
+  default = "global"
+}
+
+variable "stage" {
+  default = "default"
+}
+
+variable "name" {
+  default = "efs"
+}
+
+variable "security_groups" {
+  type = "list"
+}
+
+variable "vpc_id" {}
+
+variable "aws_region" {}
+
+variable "subnets" {
+  type = "list"
+}
+
+variable "availability_zones" {
+  type = "list"
+}
+
+variable "zone_id" {}
