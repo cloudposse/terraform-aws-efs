21928ea32dd710f39d83d18d208c62b35af7023d
aknysh <aknysh>
2017-09-27 21:21:03 -0400

    Add `mount_target_ids` output

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,7 @@ output "host" {
 output "dns_name" {
   value = "${aws_efs_file_system.default.id}.efs.${var.aws_region}.amazonaws.com"
 }
+
+output "mount_target_ids" {
+  value = ["${aws_efs_mount_target.default.*.id}"]
+}
