e82e31c3c282758d4d737b554cf4680fee7bac73
Scott Cabrinha <scabrinha@gopro.com>
2019-04-10 11:23:16 -0700

    updating readme, no need for interpolation

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ resource "aws_security_group" "default" {
 module "dns" {
   source    = "git::https://github.com/cloudposse/terraform-aws-route53-cluster-hostname.git?ref=tags/0.2.5"
   enabled   = "${local.enabled && length(var.zone_id) > 0 ? "true" : "false"}"
-  name      = "${var.dns_name == "" ? module.label.id : "${var.dns_name}"}"
+  name      = "${var.dns_name == "" ? module.label.id : var.dns_name}"
   namespace = "${var.namespace}"
   stage     = "${var.stage}"
   ttl       = 60
