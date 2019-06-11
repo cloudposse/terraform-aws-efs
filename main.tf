0fc261b97006bff939fe09daee838bc008b59bf3
Stefan Sedich <stefan.sedich@gmail.com>
2019-06-11 09:14:27 -0700

    Upgrade to TF 0.12

--- main.tf
+++ main.tf
@@ -1,52 +1,52 @@
 # Define composite variables for resources
 module "label" {
   source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.5"
-  enabled    = "${var.enabled}"
-  namespace  = "${var.namespace}"
-  name       = "${var.name}"
-  stage      = "${var.stage}"
-  delimiter  = "${var.delimiter}"
-  attributes = "${var.attributes}"
-  tags       = "${var.tags}"
+  enabled    = var.enabled
+  namespace  = var.namespace
+  name       = var.name
+  stage      = var.stage
+  delimiter  = var.delimiter
+  attributes = var.attributes
+  tags       = var.tags
 }
 
 locals {
-  enabled  = "${var.enabled == "true"}"
+  enabled  = var.enabled == "true"
   dns_name = "${join("", aws_efs_file_system.default.*.id)}.efs.${var.aws_region}.amazonaws.com"
 }
 
 resource "aws_efs_file_system" "default" {
-  count                           = "${local.enabled ? 1 : 0}"
-  tags                            = "${module.label.tags}"
-  encrypted                       = "${var.encrypted}"
-  performance_mode                = "${var.performance_mode}"
-  provisioned_throughput_in_mibps = "${var.provisioned_throughput_in_mibps}"
-  throughput_mode                 = "${var.throughput_mode}"
+  count                           = local.enabled ? 1 : 0
+  tags                            = module.label.tags
+  encrypted                       = var.encrypted
+  performance_mode                = var.performance_mode
+  provisioned_throughput_in_mibps = var.provisioned_throughput_in_mibps
+  throughput_mode                 = var.throughput_mode
 }
 
 resource "aws_efs_mount_target" "default" {
-  count           = "${local.enabled && length(var.availability_zones) > 0 ? length(var.availability_zones) : 0}"
-  file_system_id  = "${join("", aws_efs_file_system.default.*.id)}"
-  ip_address      = "${var.mount_target_ip_address}"
-  subnet_id       = "${element(var.subnets, count.index)}"
-  security_groups = ["${join("", aws_security_group.default.*.id)}"]
+  count           = local.enabled && length(var.availability_zones) > 0 ? length(var.availability_zones) : 0
+  file_system_id  = join("", aws_efs_file_system.default.*.id)
+  ip_address      = var.mount_target_ip_address
+  subnet_id       = element(var.subnets, count.index)
+  security_groups = [join("", aws_security_group.default.*.id)]
 }
 
 resource "aws_security_group" "default" {
-  count       = "${local.enabled ? 1 : 0}"
-  name        = "${module.label.id}"
+  count       = local.enabled ? 1 : 0
+  name        = module.label.id
   description = "EFS"
-  vpc_id      = "${var.vpc_id}"
+  vpc_id      = var.vpc_id
 
   lifecycle {
     create_before_destroy = true
   }
 
   ingress {
-    from_port       = "2049"                     # NFS
+    from_port       = "2049" # NFS
     to_port         = "2049"
     protocol        = "tcp"
-    security_groups = ["${var.security_groups}"]
+    security_groups = var.security_groups
   }
 
   egress {
@@ -56,16 +56,17 @@ resource "aws_security_group" "default" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = "${module.label.tags}"
+  tags = module.label.tags
 }
 
 module "dns" {
   source    = "git::https://github.com/cloudposse/terraform-aws-route53-cluster-hostname.git?ref=tags/0.2.5"
-  enabled   = "${local.enabled && length(var.zone_id) > 0 ? "true" : "false"}"
-  name      = "${var.dns_name == "" ? module.label.id : var.dns_name}"
-  namespace = "${var.namespace}"
-  stage     = "${var.stage}"
+  enabled   = local.enabled && length(var.zone_id) > 0 ? "true" : "false"
+  name      = var.dns_name == "" ? module.label.id : var.dns_name
+  namespace = var.namespace
+  stage     = var.stage
   ttl       = 60
-  zone_id   = "${var.zone_id}"
-  records   = ["${local.dns_name}"]
+  zone_id   = var.zone_id
+  records   = [local.dns_name]
 }
+
--- variables.tf
+++ variables.tf
@@ -1,84 +1,84 @@
 variable "enabled" {
-  type        = "string"
+  type        = string
   description = "Set to false to prevent the module from creating any resources"
   default     = "true"
 }
 
 variable "namespace" {
-  type        = "string"
+  type        = string
   description = "Namespace (_e.g._ `eg` or `cp`)"
   default     = "eg"
 }
 
 variable "stage" {
-  type        = "string"
+  type        = string
   description = "Stage (_e.g._ `prod`, `dev`, `staging`)"
   default     = "default"
 }
 
 variable "name" {
-  type        = "string"
+  type        = string
   description = "Name (_e.g._ `app`)"
   default     = "app"
 }
 
 variable "security_groups" {
-  type        = "list"
+  type        = list(string)
   description = "Security group IDs to allow access to the EFS"
 }
 
 variable "vpc_id" {
-  type        = "string"
+  type        = string
   description = "VPC ID"
 }
 
 variable "aws_region" {
-  type        = "string"
+  type        = string
   description = "AWS Region"
 }
 
 variable "subnets" {
-  type        = "list"
+  type        = list(string)
   description = "Subnet IDs"
 }
 
 variable "availability_zones" {
-  type        = "list"
+  type        = list(string)
   description = "Availability Zone IDs"
 }
 
 variable "zone_id" {
-  type        = "string"
+  type        = string
   description = "Route53 DNS zone ID"
   default     = ""
 }
 
 variable "delimiter" {
-  type        = "string"
+  type        = string
   description = "Delimiter to be used between `namespace`, `stage`, `name` and `attributes`"
   default     = "-"
 }
 
 variable "attributes" {
-  type        = "list"
+  type        = list(string)
   description = "Additional attributes (e.g. `1`)"
   default     = []
 }
 
 variable "tags" {
-  type        = "map"
+  type        = map(string)
   description = "Additional tags (e.g. `{ BusinessUnit = \"XYZ\" }`"
   default     = {}
 }
 
 variable "encrypted" {
-  type        = "string"
+  type        = string
   description = "If true, the disk will be encrypted"
   default     = "false"
 }
 
 variable "performance_mode" {
-  type        = "string"
+  type        = string
   description = "The file system performance mode. Can be either `generalPurpose` or `maxIO`"
   default     = "generalPurpose"
 }
@@ -89,19 +89,20 @@ variable "provisioned_throughput_in_mibps" {
 }
 
 variable "throughput_mode" {
-  type        = "string"
+  type        = string
   description = "Throughput mode for the file system. Defaults to bursting. Valid values: bursting, provisioned. When using provisioned, also set provisioned_throughput_in_mibps"
   default     = "bursting"
 }
 
 variable "mount_target_ip_address" {
-  type        = "string"
+  type        = string
   description = "The address (within the address range of the specified subnet) at which the file system may be mounted via the mount target"
   default     = ""
 }
 
 variable "dns_name" {
-  type        = "string"
+  type        = string
   description = "Name of the CNAME record to create."
   default     = ""
 }
+
