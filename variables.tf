edcc5473acda901784dc28a25ba7ae4d6c3f9e18
Maxim Mironenko <simixido@gmail.com>
2020-03-27 16:36:35 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -115,6 +115,6 @@ variable "dns_name" {
 
 variable "transition_to_ia" {
   type        = string
-  description = "Indicates how long it takes to transition files to the IA storage class."
+  description = "Indicates how long it takes to transition files to the IA storage class. Valid values: AFTER_7_DAYS, AFTER_14_DAYS, AFTER_30_DAYS, AFTER_60_DAYS and AFTER_90_DAYS"
   default     = ""
 }
