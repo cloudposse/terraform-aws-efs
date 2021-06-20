5f4418b2d736fe53648188183d59a445b8799660
root <root@ip-172-31-35-51.ec2.internal>
2021-06-20 08:46:56 +0000

    removed for_each

--- main.tf
+++ main.tf
@@ -17,10 +17,7 @@ resource "aws_efs_file_system" "default" {
     content {
       transition_to_ia = var.transition_to_ia
     }
-    for_each = var.prevent_destroy ? [1] : []
-    content {
-      prevent_destroy  = var.prevent_destroy
-    }
+    prevent_destroy  = var.prevent_destroy
   }
 }
 
