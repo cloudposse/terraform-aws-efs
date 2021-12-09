eb334b151e87f1d33a293008eaca7cd0d28b690c
Dragan Dragicevic <dragan.dragicevic@gls-itservices.com>
2021-12-09 09:32:27 +0100

    fix security group issue when security_group is null

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -32,48 +32,34 @@ module "efs" {
   vpc_id  = module.vpc.vpc_id
   subnets = module.subnets.private_subnet_ids
 
-  access_points = {
-    "data" = {
-      posix_user = {
-        gid            = "1001"
-        uid            = "5000"
-        secondary_gids = "1002,1003"
-      }
-      creation_info = {
-        gid         = "1001"
-        uid         = "5000"
-        permissions = "0755"
-      }
-    }
-    "data2" = {
-      posix_user = {
-        gid            = "2001"
-        uid            = "6000"
-        secondary_gids = null
-      }
-      creation_info = {
-        gid         = "123"
-        uid         = "222"
-        permissions = "0555"
-      }
-    }
-  }
+  efs_backup_policy_enabled       = true
 
-  additional_security_group_rules = [
-    {
-      type                     = "ingress"
-      from_port                = 2049
-      to_port                  = 2049
-      protocol                 = "tcp"
-      cidr_blocks              = []
-      source_security_group_id = module.vpc.vpc_default_security_group_id
-      description              = "Allow ingress traffic to EFS from trusted Security Groups"
-    }
-  ]
+  associated_security_group_ids   = [module.efs_security_group.id]
+  create_security_group           = false
 
   transition_to_ia = ["AFTER_7_DAYS"]
 
   security_group_create_before_destroy = false
 
   context = module.this.context
+}
+
+  module "efs_security_group" {
+  source = "cloudposse/security-group/aws"
+
+  name                       = "prod_efs_security_group"
+  security_group_name        = ["prod_efs_security_group"]
+  security_group_description = "Security Group for EFS for prod hosts"
+  vpc_id                     = module.vpc.vpc_id
+
+  rules = [
+    {
+      type                     = "ingress"
+      from_port                = 2049
+      to_port                  = 2049
+      protocol                 = "TCP"
+      source_security_group_id = module.vpc.vpc_default_security_group_id
+      description              = "Allow EC2 instances to get access on EFS over port 2049"
+    }
+  ]
 }
