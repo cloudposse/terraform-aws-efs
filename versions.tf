6e4f9ba9d40ccd8c14fb287aaecf9d39f6c140e6
Mike Wolonkiewicz <michal@wolonkiewi.cz>
2023-11-30 13:09:12 +0100

    versions.tf: bump hashicorp/aws and terraform
    examples/complete/versions.tf: bump hashicorp/aws and terraform

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.14.0"
+  required_version = ">= 1.1.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.59.0"
+      version = ">= 4.42.0"
     }
   }
 }
